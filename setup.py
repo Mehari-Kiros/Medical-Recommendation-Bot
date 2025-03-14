20d139b835e6049b3f1f62e9ff6c0e821756e5c1
Mehari Kiros <meharik1219@gmail.com>
2025-03-14 15:46:06 +0300

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Medical Chatbot',
+    version= '0.0.0',
+    author= 'Mehari-Kiros',
+    author_email= 'meharik1219@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
